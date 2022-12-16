8990b919baad15532846edb5f9353e6a2668365c
NNEUID <nneuid@gmail.com>
2022-12-16 08:22:58 +0000

    attr/data binding

--- app.js
+++ app.js
@@ -13,18 +13,22 @@ const app = Vue.createApp({
     return {
       showBooks: true,
       message: 'Hello Vue :)',
+      url: 'https://nneuid.com',
       books: [
         {
           title: 'name of the wind',
-          author: 'Patrick Rothfuss'
+          author: 'Patrick Rothfuss',
+          cover: 'https://raw.githubusercontent.com/iamshaunjp/Vue-3-Firebase/lesson-13/assets/1.jpg'
         },
         {
           title: 'the way of kings',
-          author: 'Brandon Sanderson'
+          author: 'Brandon Sanderson',
+          cover: 'https://raw.githubusercontent.com/iamshaunjp/Vue-3-Firebase/lesson-13/assets/2.jpg'
         },
         {
           title: 'The final empire',
-          author: 'Brandon Sanderson'
+          author: 'Brandon Sanderson',
+          cover: 'https://raw.githubusercontent.com/iamshaunjp/Vue-3-Firebase/lesson-13/assets/3.jpg'
         }
       ],
     }
